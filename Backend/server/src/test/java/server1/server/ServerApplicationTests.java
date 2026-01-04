9b5ccf8b4d2190193fd7b805a51f02ac15a93bd9
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2026-01-04 13:20:40 +0200

    added backend boilerplate code

---
+++ Backend/server/src/test/java/server1/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server1.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
